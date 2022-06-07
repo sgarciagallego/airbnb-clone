e22dd29132af9db2624bfecb1957f5adfab6d1bb
sgarciagallego <gallegogarciasergio@gmail.com>
2022-06-07 16:45:21 +0100

    v0.2.0 | [Making property items other data types] - not just string

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -9,27 +9,27 @@ const CardContainer = () => {
     <section className='card--container'>
       <Card 
         img={KatieZaferes}
-        reviewRating='5.0'
-        reviewCount='6'
+        reviewRating={5.0}
+        reviewCount={6}
         location='USA'
         description='Life lessons with Katie Zaferes'
-        price='136'
+        price={136}
       />
       <Card 
         img={WeddingPhotography}
-        reviewRating='5.0'
-        reviewCount='30'
+        reviewRating={5.0}
+        reviewCount={30}
         location='USA'
         description='Professional wedding day photography'
-        price='125'
+        price={125}
       />
       <Card 
         img={MountainBiking}
-        reviewRating='4.8'
-        reviewCount='2'
+        reviewRating={4.8}
+        reviewCount={2}
         location='USA'
         description='Group Mountain Biking tour'
-        price='50'
+        price={50}
       />
     </section>
   )
